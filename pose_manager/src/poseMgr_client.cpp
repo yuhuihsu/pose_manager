d14bfc2fee74b5831fbf159725ee9b7b269e88b3
yuhuihsu <yuhui.hsu2017@gmail.com>
2017-03-21 10:49:20 +0800

    Add files via upload

--- pose_manager/src/poseMgr_client.cpp
+++ pose_manager/src/poseMgr_client.cpp
@@ -1,50 +1,283 @@
-#include "ros/ros.h"
-#include "pose_manager/add.h"
-#include "pose_manager/LocationArray.h"
 #include "pose_manager/Location.h"
+#include "pose_manager/LocationArray.h"
+#include "pose_manager/insert.h"
+#include "pose_manager/deleteName.h"
+#include "pose_manager/deletePose.h"
+#include "pose_manager/deleteAll.h"
+#include "pose_manager/queryName.h"
+#include "pose_manager/queryPose.h"
+#include "pose_manager/queryAll.h"
+#include "pose_manager/update.h"
+#include "ros/ros.h"
 #include <cstdlib>
 
-int main(int argc, char **argv)
+void insert()
 {
-  ros::init(argc, argv, "poseMgr_client");
-  if (argc != 3)
-  {
-    ROS_INFO("usage: poseMgr_client X Y");
-    return 1;
-  }
-
+  /** Insert **/
   ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<pose_manager::add>("add_location");
-  pose_manager::add srv;
+  ros::ServiceClient client = n.serviceClient<pose_manager::insert>("insert_location");
+  pose_manager::insert srv;
 
   pose_manager::LocationArray locationArray;
   pose_manager::Location location;
   location.pose.position.x = 3;
   location.pose.position.y = 4;
-  location.name = "hihi";
+  location.name = "livingroom";
   locationArray.location.push_back(location);
-  
+
   pose_manager::Location location1;
-  location1.pose.position.x = 3;
-  location1.pose.position.y = 4;
-  location1.name = "kerker";
+  location1.pose.position.x = 6;
+  location1.pose.position.y = 9;
+  location1.name = "kitchen";
   locationArray.location.push_back(location1);
 
   srv.request.location = locationArray;
-  ROS_INFO("XDDDD");
 
   if (client.call(srv))
   {
-    ROS_INFO("hihi finished");
+    ROS_INFO("Inset finished");
+    ROS_INFO("result : %d", srv.response.result);
   }
   else
   {
-    ROS_INFO("XDDDD222");
-    
-    ROS_ERROR("Failed to call service add_location");
+    ROS_ERROR("Failed to call service insert_location");
+  }
+}
+
+void deleteName()
+{
+  /** Delete by Name **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::deleteName>("delete_location_by_name");
+  pose_manager::deleteName deleteName_srv;
+  deleteName_srv.request.name = "livingroom";
+  if (client.call(deleteName_srv))
+  {
+    ROS_INFO("Delete name finished");
+    ROS_INFO("result : %d", deleteName_srv.response.result);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service delete_location_by_name");
+  }
+}
+
+void deletePose()
+{
+  /** Delete by Pose **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::deletePose>("delete_location_by_pose");
+  pose_manager::deletePose deletePose_srv;
+  geometry_msgs::Pose pose;
+  pose.position.x = 3;
+  pose.position.y = 4;
+  pose.position.z = 0;
+  pose.orientation.x = 0;
+  pose.orientation.y = 0;
+  pose.orientation.z = 0;
+  pose.orientation.w = 0;
+
+  deletePose_srv.request.pose = pose;
+
+  if (client.call(deletePose_srv))
+  {
+    ROS_INFO("Delete Pose finished");
+    ROS_INFO("result : %d", deletePose_srv.response.result);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service delete_location_by_name");
+  }
+}
+
+void deleteAll()
+{
+  /** Delete all **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::deleteAll>("delete_All");
+  pose_manager::deleteAll deleteAll_srv;
+  // deleteAll_srv.request.execute = true;
+
+  if (client.call(deleteAll_srv))
+  {
+    ROS_INFO("Delete finished");
+    if (deleteAll_srv.response.result)
+    {
+      ROS_INFO("Delete succeed");
+    }
+    else
+    {
+      ROS_INFO("Delete failed");
+    }
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service delete_All");
+  }
+}
+void queryName()
+{
+  /** Query Name **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::queryName>("query_Name");
+  pose_manager::queryName queryName_srv;
+
+  geometry_msgs::Pose pose;
+  pose.position.x = 3;
+  pose.position.y = 4;
+  pose.position.z = 0;
+  pose.orientation.x = 0;
+  pose.orientation.y = 0;
+  pose.orientation.z = 0;
+  pose.orientation.w = 0;
+  queryName_srv.request.pose = pose;
+
+  if (client.call(queryName_srv))
+  {
+    ROS_INFO("Query finished");
+    if (queryName_srv.response.result)
+    {
+      ROS_INFO("Name : %s", queryName_srv.response.name.c_str());
+    }
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service query_Pose");
+  }
+}
+
+void queryPose()
+{
+  /** Query Pose **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::queryPose>("query_Pose");
+  pose_manager::queryPose queryPose_srv;
+
+  queryPose_srv.request.name = "kitchen";
+  if (client.call(queryPose_srv))
+  {
+    ROS_INFO("Query finished");
+    if (queryPose_srv.response.result)
+    {
+      ROS_INFO("pose position x %f", queryPose_srv.response.pose.position.x);
+      ROS_INFO("pose position y %f", queryPose_srv.response.pose.position.y);
+      ROS_INFO("pose position z %f", queryPose_srv.response.pose.position.z);
+      ROS_INFO("pose orientation x %f", queryPose_srv.response.pose.orientation.x);
+      ROS_INFO("pose orientation y %f", queryPose_srv.response.pose.orientation.y);
+      ROS_INFO("pose orientation z %f", queryPose_srv.response.pose.orientation.z);
+      ROS_INFO("pose posiorientationtion w %f", queryPose_srv.response.pose.orientation.w);
+    }
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service query_Pose");
+  }
+}
+
+void queryAll()
+{
+  /** Query all **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::queryAll>("query_All");
+  pose_manager::queryAll queryAll_srv;
+
+  queryAll_srv.request.query = true;
+  ROS_INFO("XDDDDDDDDDDD111");
+
+  if (client.call(queryAll_srv))
+  {
+    ROS_INFO("Query finished");
+    // if (queryAll_srv.response.result){
+    //   ROS_INFO("XDDDDDDDDDDD");
+    // }
+    if (queryAll_srv.response.result)
+    {
+
+      int iSize = queryAll_srv.response.locationArray.location.size();
+      ROS_INFO("iSize %d\n", iSize);
+      for (int iNum = 0; iNum < iSize; iNum++)
+      {
+        ROS_INFO("name %s", queryAll_srv.response.locationArray.location[iNum].name.c_str());
+        ROS_INFO("pose position x %f", queryAll_srv.response.locationArray.location[iNum].pose.position.x);
+        ROS_INFO("pose position y %f", queryAll_srv.response.locationArray.location[iNum].pose.position.y);
+        ROS_INFO("pose position z %f", queryAll_srv.response.locationArray.location[iNum].pose.position.z);
+        ROS_INFO("pose orientation x %f", queryAll_srv.response.locationArray.location[iNum].pose.orientation.x);
+        ROS_INFO("pose orientation y %f", queryAll_srv.response.locationArray.location[iNum].pose.orientation.y);
+        ROS_INFO("pose orientation z %f", queryAll_srv.response.locationArray.location[iNum].pose.orientation.z);
+        ROS_INFO("pose posiorientationtion w %f", queryAll_srv.response.locationArray.location[iNum].pose.orientation.w);
+      }
+    }
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service query_Pose");
+  }
+}
+
+void update()
+{
+  /** Update **/
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<pose_manager::update>("update_location");
+  pose_manager::update update_srv;
+  pose_manager::LocationArray locationArray;
+  pose_manager::Location location;
+
+  location.pose.position.x = 36;
+  location.pose.position.y = 46;
+  location.pose.position.z = 50;
+  location.name = "livingroom";
+  locationArray.location.push_back(location);
+  update_srv.request.location = locationArray;
+  if (client.call(update_srv))
+  {
+    ROS_INFO("Update finished");
+    ROS_INFO("result : %d", update_srv.response.result);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service update_location");
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "poseMgr_client");
+  if (argc != 2)
+  {
+    ROS_INFO("usage: poseMgr_client $mode \nMode selection\n");
+    ROS_INFO("1.insert  \n2.deleteName  \n3.deletePose  \n4.deleteAll");
+    ROS_INFO("5.queryName  \n6.queryPose  \n7.queryAll \n8.update");
     return 1;
   }
 
+  switch (atoll(argv[1]))
+  {
+  case 1:
+    insert();
+    break;
+  case 2:
+    deleteName();
+    break;
+  case 3:
+    deletePose();
+    break;
+  case 4:
+    deleteAll();
+    break;
+  case 5:
+    queryName();
+    break;
+  case 6:
+    queryPose();
+    break;
+  case 7:
+    queryAll();
+    break;
+  case 8:
+    update();
+    break;
+  }
 
   return 0;
 }
--- pose_manager/src/poseMgr_service.cpp
+++ pose_manager/src/poseMgr_service.cpp
@@ -1,97 +1,502 @@
-#include "ros/ros.h"
-#include "pose_manager/LocationArray.h"
 #include "pose_manager/Location.h"
-#include "pose_manager/add.h"
-#include <stdio.h>
-#include <fstream>
-#include <iterator>
-#include <string>
+#include "pose_manager/LocationArray.h"
+#include "pose_manager/insert.h"
+#include "pose_manager/deleteName.h"
+#include "pose_manager/deletePose.h"
+#include "pose_manager/deleteAll.h"
+#include "pose_manager/queryName.h"
+#include "pose_manager/queryPose.h"
+#include "pose_manager/queryAll.h"
+#include "pose_manager/update.h"
+#include "ros/ros.h"
 #include <cstdlib>
-#include <sstream>
 #include <iostream>
-#include <fstream>
 #include <sqlite3.h>
+#include <sstream>
+#include <string>
 
-bool writeFile(pose_manager::LocationArray locationArray)
+const bool DBG = true;
+const std::string TableName = "LOCATION";
+const char *DBName = "poseMgr.db";
+const float UNKNOWN = -10000.0;
+
+std::string convertToString(float fOri)
 {
-  std::fstream fout;
-  fout.open("test.txt", std::ios::out | std::ios::app);
-  if (fout.fail())
-  {
-    ROS_INFO("open file failed");
-    return false;
-  }
-
-  for (int iNum = 0; iNum < locationArray.location.size(); iNum++)
-  {
-    std::string result;
-    std::ostringstream ss;
-
-    result += locationArray.location[iNum].name;
-
-    ss << locationArray.location[iNum].pose.position.x;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.position.y;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.position.z;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.orientation.x;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.orientation.y;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.orientation.z;
-    ss << "\t";
-    ss << locationArray.location[iNum].pose.orientation.w;
-    ss << "\t";
-    result += "\t" + (ss.str());
-    fout << result << std::endl;
-  }
-  fout.close();
-  return true;
+  std::string sResult;
+  std::ostringstream ss;
+  ss << fOri;
+  return ss.str();
 }
 
-bool add(pose_manager::add::Request &req,
-         pose_manager::add::Response &res)
-{
-  ROS_INFO("Receive request");
-  int iSize = 0;
-  iSize = req.location.location.size();
-  ROS_INFO("size: %d", iSize);
-  res.result = true;
-  writeFile(req.location);
-
-  return true;
-}
-
-bool createTable(sqlite3 *db)
+bool insertQuery(pose_manager::Location location, sqlite3 *db)
 {
   char *errMsg = NULL;
-  char *sql = "CREATE TABLE LOCATION(" \ 
-              "ID INT PRIMARY KEY NOT NULL," \
-              "NAME TEXT NOT NULL," \
-              "POSE_X FLOAT NOT NULL" \
-              "POSE_Y FLOAT NOT NULL" \
-              "POSE_Z FLOAT NOT NULL" \
-              "ORIENTATION_X FLOAT NOT NULL" \
-              "ORIENTATION_Y FLOAT NOT NULL" \
-              "ORIENTATION_Z FLOAT NOT NULL" \
-              "ORIENTATION_W FLOAT NOT NULL";
-  sqlite3_exec(db, sql, 0, 0, &errMsg);
+  const char *sql;
+  std::string sResult;
 
+  sResult += "INSERT INTO " + TableName + " VALUES(";
+  sResult += "\"" + location.name + "\", ";
+  sResult += convertToString(location.pose.position.x) + ", ";
+  sResult += convertToString(location.pose.position.y) + ", ";
+  sResult += convertToString(location.pose.position.z) + ", ";
+  sResult += convertToString(location.pose.orientation.x) + ", ";
+  sResult += convertToString(location.pose.orientation.y) + ", ";
+  sResult += convertToString(location.pose.orientation.z) + ", ";
+  sResult += convertToString(location.pose.orientation.w) + ")";
+  sql = sResult.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Insert ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    return false;
+  }
+  return true;
 }
 
-bool createDB()
+bool insert(pose_manager::insert::Request &req,
+            pose_manager::insert::Response &res)
+{
+  sqlite3 *db;
+  int iSize = 0;
+  bool result = true;
+  iSize = req.location.location.size();
+  sqlite3_open(DBName, &db);
+  for (int iNum = 0; iNum < iSize; iNum++)
+  {
+    result = insertQuery(req.location.location[iNum], db);
+    result |= result;
+  }
+  sqlite3_close(db);
+  res.result = result;
+
+  return true;
+}
+
+bool updateQuery(pose_manager::Location location, sqlite3 *db)
+{
+  char *errMsg = NULL;
+  const char *sql;
+  std::string sQuery;
+  char **result;
+  int iRows, iCols, iIndex;
+
+  sQuery += "SELECT * FROM " + TableName + " WHERE NAME = \"" + location.name + "\" AND ";
+  sQuery += "POSE_X = " + convertToString(location.pose.position.x) + " AND ";
+  sQuery += "POSE_Y = " + convertToString(location.pose.position.y) + " AND ";
+  sQuery += "POSE_Z = " + convertToString(location.pose.position.z) + " AND ";
+  sQuery += "ORIENTATION_X = " + convertToString(location.pose.orientation.x) + " AND ";
+  sQuery += "ORIENTATION_Y = " + convertToString(location.pose.orientation.y) + " AND ";
+  sQuery += "ORIENTATION_Z = " + convertToString(location.pose.orientation.z) + " AND ";
+  sQuery += "ORIENTATION_W = " + convertToString(location.pose.orientation.w);
+  sql = sQuery.c_str();
+  fprintf(stderr, "sql %s\n", sql);
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    return false;
+  }
+  else
+  {
+    sqlite3_get_table(db, sql, &result, &iRows, &iCols, &errMsg);
+    if (iRows == iCols && iRows == 0 && iCols == 0)
+    {
+      fprintf(stderr, "\t> Error: no data\n");
+      return false;
+    }
+  }
+  sQuery = "";
+  sQuery += "UPDATE " + TableName + " SET ";
+  sQuery += "POSE_X = " + convertToString(location.pose.position.x) + ", ";
+  sQuery += "POSE_Y = " + convertToString(location.pose.position.y) + ", ";
+  sQuery += "POSE_Z = " + convertToString(location.pose.position.z) + ", ";
+  sQuery += "ORIENTATION_X = " + convertToString(location.pose.orientation.x) + ", ";
+  sQuery += "ORIENTATION_Y = " + convertToString(location.pose.orientation.y) + ", ";
+  sQuery += "ORIENTATION_Z = " + convertToString(location.pose.orientation.z) + ", ";
+  sQuery += "ORIENTATION_W = " + convertToString(location.pose.orientation.w) + " ";
+  sQuery += "WHERE NAME = \"" + location.name + "\"";
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Update ==\n");
+    fprintf(stderr, "sql %s\n", sql);
+  }
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    return false;
+  }
+  return true;
+}
+
+bool update(pose_manager::update::Request &req,
+            pose_manager::update::Response &res)
+{
+  sqlite3 *db;
+  int iSize = 0;
+  bool result = true;
+  iSize = req.location.location.size();
+  sqlite3_open(DBName, &db);
+  for (int iNum = 0; iNum < iSize; iNum++)
+  {
+    result = updateQuery(req.location.location[iNum], db);
+    result |= result;
+  }
+  sqlite3_close(db);
+  res.result = result;
+
+  return true;
+}
+
+bool deleteNameQuery(std::string name, sqlite3 *db)
+{
+  char *errMsg = NULL;
+  const char *sql;
+  std::string sQuery;
+
+  sQuery += "DELETE FROM " + TableName + " WHERE NAME = \"" + name + "\"";
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Delete Name ==\n");
+    fprintf(stderr, "sql %s\n", sql);
+  }
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    return false;
+  }
+  return true;
+}
+
+bool deletebyName(pose_manager::deleteName::Request &req,
+                  pose_manager::deleteName::Response &res)
+{
+  sqlite3 *db;
+  bool result;
+
+  sqlite3_open(DBName, &db);
+  result = deleteNameQuery(req.name, db);
+  sqlite3_close(db);
+  if (result)
+  {
+    res.result = true;
+  }
+  else
+  {
+    res.result = false;
+  }
+  return true;
+}
+
+bool deletePoseQuery(geometry_msgs::Pose pose, sqlite3 *db)
+{
+  char *errMsg = NULL;
+  const char *sql;
+  std::string sQuery;
+
+  sQuery += "DELETE FROM " + TableName + " WHERE ";
+  sQuery += "POSE_X = " + convertToString(pose.position.x) + " AND ";
+  sQuery += "POSE_Y = " + convertToString(pose.position.y) + " AND ";
+  sQuery += "POSE_Z = " + convertToString(pose.position.z) + " AND ";
+  sQuery += "ORIENTATION_X = " + convertToString(pose.orientation.x) + " AND ";
+  sQuery += "ORIENTATION_Y = " + convertToString(pose.orientation.y) + " AND ";
+  sQuery += "ORIENTATION_Z = " + convertToString(pose.orientation.z) + " AND ";
+  sQuery += "ORIENTATION_W = " + convertToString(pose.orientation.w);
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Delete Pose ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    return false;
+  }
+  return true;
+}
+
+bool deletebyPose(pose_manager::deletePose::Request &req,
+                  pose_manager::deletePose::Response &res)
+{
+  sqlite3 *db;
+  bool result;
+
+  sqlite3_open(DBName, &db);
+  result = deletePoseQuery(req.pose, db);
+  sqlite3_close(db);
+  if (result)
+  {
+    res.result = true;
+  }
+  else
+  {
+    res.result = false;
+  }
+  return true;
+}
+
+geometry_msgs::Pose queryPoseQuery(std::string sName, sqlite3 *db)
+{
+  char *errMsg = NULL;
+  const char *sql;
+  char **result;
+  int iRows, iCols, iIndex;
+  std::string sQuery;
+  geometry_msgs::Pose pose;
+
+  sQuery += "SELECT * FROM " + TableName + " WHERE NAME = \"" + sName + "\"";
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Query Pose ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  sqlite3_get_table(db, sql, &result, &iRows, &iCols, &errMsg);
+
+  if (iRows != 0 && iCols != 0)
+  {
+    iIndex = iCols;
+    iIndex++;
+    pose.position.x = atof(result[iIndex]);
+    iIndex++;
+    pose.position.y = atof(result[iIndex]);
+    iIndex++;
+    pose.position.z = atof(result[iIndex]);
+    iIndex++;
+    pose.orientation.x = atof(result[iIndex]);
+    iIndex++;
+    pose.orientation.y = atof(result[iIndex]);
+    iIndex++;
+    pose.orientation.z = atof(result[iIndex]);
+    iIndex++;
+    pose.orientation.w = atof(result[iIndex]);
+  }
+  else
+  {
+    pose.position.x = UNKNOWN;
+  }
+  sqlite3_free_table(result);
+  return pose;
+}
+
+bool queryPose(pose_manager::queryPose::Request &req,
+               pose_manager::queryPose::Response &res)
+{
+  sqlite3 *db;
+  geometry_msgs::Pose pose;
+  bool result;
+
+  sqlite3_open(DBName, &db);
+  pose = queryPoseQuery(req.name, db);
+  sqlite3_close(db);
+  if (pose.position.x != UNKNOWN)
+  {
+    res.pose = pose;
+    res.result = true;
+  }
+  else
+  {
+    res.result = false;
+  }
+  return true;
+}
+
+std::string queryNameQuery(geometry_msgs::Pose pose, sqlite3 *db)
+{
+  char *errMsg = NULL;
+  const char *sql;
+  char **result;
+  int iRows, iCols, iIndex;
+  std::string sQuery;
+  std::string sName;
+
+  sQuery += "SELECT NAME FROM " + TableName + " WHERE ";
+  sQuery += "POSE_X = " + convertToString(pose.position.x) + " AND ";
+  sQuery += "POSE_Y = " + convertToString(pose.position.y) + " AND ";
+  sQuery += "POSE_Z = " + convertToString(pose.position.z) + " AND ";
+  sQuery += "ORIENTATION_X = " + convertToString(pose.orientation.x) + " AND ";
+  sQuery += "ORIENTATION_Y = " + convertToString(pose.orientation.y) + " AND ";
+  sQuery += "ORIENTATION_Z = " + convertToString(pose.orientation.z) + " AND ";
+  sQuery += "ORIENTATION_W = " + convertToString(pose.orientation.w);
+
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Query Name ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  sqlite3_get_table(db, sql, &result, &iRows, &iCols, &errMsg);
+  if (iRows != 0 && iCols != 0)
+  {
+    iIndex = iCols;
+    sName = result[iIndex];
+  }
+  else
+  {
+    return "";
+  }
+  return sName;
+}
+
+bool queryName(pose_manager::queryName::Request &req,
+               pose_manager::queryName::Response &res)
+{
+  sqlite3 *db;
+  std::string sName;
+
+  sqlite3_open(DBName, &db);
+
+  sName = queryNameQuery(req.pose, db);
+  if (sName != "")
+  {
+    res.name = sName;
+    res.result = true;
+  }
+  else
+  {
+    res.result = false;
+  }
+  sqlite3_close(db);
+  return true;
+}
+
+pose_manager::LocationArray queryAllQuery(sqlite3 *db)
+{
+  pose_manager::LocationArray locationArray;
+  char *errMsg = NULL;
+  const char *sql;
+  char **result;
+  int iRows, iCols, iIndex;
+  std::string sQuery;
+  geometry_msgs::Pose pose;
+  pose_manager::Location location;
+
+  sQuery += "SELECT * FROM " + TableName;
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Query all ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  sqlite3_get_table(db, sql, &result, &iRows, &iCols, &errMsg);
+
+  if (iRows != 0 && iCols != 0)
+  {
+    iIndex = iCols;
+
+    for (int iRow = 0; iRow < iRows; iRow++)
+    {
+      location.name = result[iIndex];
+      iIndex++;
+      pose.position.x = atof(result[iIndex]);
+      iIndex++;
+      pose.position.y = atof(result[iIndex]);
+      iIndex++;
+      pose.position.z = atof(result[iIndex]);
+      iIndex++;
+      pose.orientation.x = atof(result[iIndex]);
+      iIndex++;
+      pose.orientation.y = atof(result[iIndex]);
+      iIndex++;
+      pose.orientation.z = atof(result[iIndex]);
+      iIndex++;
+      pose.orientation.w = atof(result[iIndex]);
+      iIndex++;
+      location.pose = pose;
+      locationArray.location.push_back(location);
+    }
+  }
+  else
+  {
+    location.name = "";
+    locationArray.location.push_back(location);
+  }
+  sqlite3_free_table(result);
+  return locationArray;
+}
+
+bool queryAll(pose_manager::queryAll::Request &req,
+              pose_manager::queryAll::Response &res)
+{
+  sqlite3 *db;
+  pose_manager::LocationArray locationArray;
+
+  sqlite3_open(DBName, &db);
+  locationArray = queryAllQuery(db);
+  if (locationArray.location[0].name != "")
+  {
+    res.locationArray = locationArray;
+    res.result = true;
+  }
+  else
+  {
+    res.result = false;
+  }
+  sqlite3_close(db);
+  return true;
+}
+
+bool deleteAll(pose_manager::deleteAll::Request &req,
+               pose_manager::deleteAll::Response &res)
+{
+  sqlite3 *db;
+  char *errMsg = NULL;
+  const char *sql;
+  std::string sQuery;
+
+  pose_manager::LocationArray locationArray;
+  sqlite3_open(DBName, &db);
+
+  sQuery += "DELETE FROM " + TableName;
+  sql = sQuery.c_str();
+  if (DBG)
+  {
+    fprintf(stderr, "== Delete all ==\n");
+    fprintf(stderr, "sql %s\n\n", sql);
+  }
+  if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK)
+  {
+    fprintf(stderr, "\t> Error: %s\n", errMsg);
+    res.result = false;
+  }
+  res.result = true;
+  return true;
+}
+
+void createTable(sqlite3 *db)
+{
+  const char *sql;
+  char *errMsg = NULL;
+  std::string create = "CREATE TABLE " + TableName + " ("
+                                                     "NAME TEXT PRIMARY KEY NOT NULL,"
+                                                     "POSE_X FLOAT NOT NULL,"
+                                                     "POSE_Y FLOAT NOT NULL,"
+                                                     "POSE_Z FLOAT NOT NULL,"
+                                                     "ORIENTATION_X FLOAT NOT NULL,"
+                                                     "ORIENTATION_Y FLOAT NOT NULL,"
+                                                     "ORIENTATION_Z FLOAT NOT NULL,"
+                                                     "ORIENTATION_W FLOAT NOT NULL)";
+
+  sql = create.c_str();
+  sqlite3_exec(db, sql, 0, 0, &errMsg);
+}
+
+void createDB()
 {
   sqlite3 *db;
   char *zErrMsg = 0;
   int rc;
 
-  rc = sqlite3_open("test.db", &db);
+  rc = sqlite3_open(DBName, &db);
 
   if (rc)
   {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    return (0);
   }
   else
   {
@@ -106,9 +511,17 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "poseMgr_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("add_location", add);
-  ROS_INFO("Ready to add two ints.");
+  ros::ServiceServer insert_service = n.advertiseService("insert_location", insert);
+  ros::ServiceServer update_service = n.advertiseService("update_location", update);
+  ros::ServiceServer deleteName_service = n.advertiseService("delete_location_by_name", deletebyName);
+  ros::ServiceServer deletePose_service = n.advertiseService("delete_location_by_pose", deletebyPose);
+  ros::ServiceServer deleteAll_service = n.advertiseService("delete_All", deleteAll);
+  ros::ServiceServer queryPose_service = n.advertiseService("query_Pose", queryPose);
+  ros::ServiceServer queryName_service = n.advertiseService("query_Name", queryName);
+  ros::ServiceServer queryAll_service = n.advertiseService("query_All", queryAll);
+
   createDB();
+  ROS_INFO("Service ready\n");
   ros::spin();
   return 0;
 }
